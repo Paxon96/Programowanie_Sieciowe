b7e31d3cc95fedf4f2477fae5613333187a54fe8
Paxon96 <bwloczyk@gmail.com>
2019-05-20 13:39:03 +0200

    Work Lab2

---
+++ Lab2/MultiThreating1/MultiThreating1/Program1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreating1 {
+    class Program1 {
+
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(HelloWorld);
+
+            
+            thread.Start();
+            thread.Join();
+        }
+    }
+}
---
+++ Lab2/MultiThreating2/MultiThreating2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+
+namespace MultiThreating2 {
+    class Program {
+
+        static void SimpleThread(string number)
+        {
+            Console.WriteLine((char)65);
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(() => SimpleThread("1"));
+            t.Start();
+
+
+            Console.Read();
+        }
+    }
+}
